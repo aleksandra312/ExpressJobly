d9a6064cd3133c372c4ef3b333215f0bdf41060c
aleksandra <oleksandra.klevchuk@gmail.com>
2021-10-31 12:51:11 -0500

    fix user model tests.

--- models/_testCommon.js
+++ models/_testCommon.js
@@ -16,6 +16,15 @@ async function commonBeforeAll() {
            ('c2', 'C2', 2, 'Desc2', 'http://c2.img'),
            ('c3', 'C3', 3, 'Desc3', 'http://c3.img')`);
 
+    const resultsJobs = await db.query(`
+           INSERT INTO jobs (title, salary, equity, company_handle)
+           VALUES ('Job1', 100, '0.1', 'c1'),
+                  ('Job2', 200, '0.2', 'c1'),
+                  ('Job3', 300, '0', 'c1'),
+                  ('Job4', NULL, NULL, 'c1')
+           RETURNING id`);
+    testJobIds.splice(0, 0, ...resultsJobs.rows.map((r) => r.id));
+
     await db.query(
         `
         INSERT INTO users(username,
